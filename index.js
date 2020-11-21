2a9c744e63a5beffb5c1388873559b107c64c233
layla  hammoud <laylahammoud63@gmail.com>
2020-11-21 10:46:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-animal = ""
+const animal = "cat"
 
 function myAnimal() {
   return animal
@@ -12,8 +12,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  let  two = 2
+  return n + two }
 
   // Feel free to move things around!
-  two = 2
+
 }
